ca30d8670a7fe23fc499de8f734ef93b84d9f051
mikhail <m.v.dyomin@mail.ru>
2025-02-02 18:25:42 +0300

    removed by-properties. edited mojos

--- delegate-this-maven-plugin/src/main/kotlin/mikhail/dyomin/delegatethis/DelegateThisMojo.kt
+++ delegate-this-maven-plugin/src/main/kotlin/mikhail/dyomin/delegatethis/DelegateThisMojo.kt
@@ -4,18 +4,28 @@ import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.plugins.annotations.ResolutionScope
-import kotlin.io.path.Path
+import java.nio.file.Path
+import kotlin.io.path.absolute
+
+abstract class DelegateThisMojoBase : AbstractMojo() {
+    protected lateinit var directoryWithClasses: Path
+
+    override fun execute() = DelegateThis(directoryWithClasses).execute()
+}
 
 /**
  * @goal transform-delegators
  * @phase process-classes
  */
 @Mojo(name = "transform-delegators", requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
-class DelegateThisMojo : AbstractMojo() {
-    @Parameter(readonly = true, defaultValue = "\${project.build.outputDirectory}")
-    private lateinit var buildOutputDirectory: String
-
-    override fun execute() = DelegateThis(Path(buildOutputDirectory)).execute()
+class DelegateThisMojo : DelegateThisMojoBase() {
+    /**
+     * directory with `*.class` files produced by compiler (default value: `${project.build.outputDirectory}`)
+     */
+    @Parameter(readonly = false, defaultValue = "\${project.build.outputDirectory}")
+    private fun setBuildOutputDirectory(dir: String) {
+        this.directoryWithClasses = Path.of(dir).absolute()
+    }
 }
 
 /**
@@ -23,9 +33,12 @@ class DelegateThisMojo : AbstractMojo() {
  * @phase process-test-classes
  */
 @Mojo(name = "transform-delegators-test", requiresDependencyCollection = ResolutionScope.TEST)
-class DelegateThisTestsMojo : AbstractMojo() {
-    @Parameter(readonly = true, defaultValue = "\${project.build.testOutputDirectory}")
-    private lateinit var buildTestOutputDirectory: String
-
-    override fun execute() = DelegateThis(Path(buildTestOutputDirectory)).execute()
+class DelegateThisTestsMojo : DelegateThisMojoBase() {
+    /**
+     * directory with `*.class` files produced by compiler (default value: `${project.build.testOutputDirectory}`)
+     */
+    @Parameter(readonly = false, defaultValue = "\${project.build.testOutputDirectory}")
+    private fun setBuildTestOutputDirectory(dir: String) {
+        this.directoryWithClasses = Path.of(dir).absolute()
+    }
 }
